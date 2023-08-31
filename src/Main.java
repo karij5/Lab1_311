33c9d2b7a50b69f29da6a09fd68eb769fb196dfd
karij5 <karij5@farmingdale.edu>
2023-08-31 13:37:42 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println(firstBike.getEngineSize());
         System.out.println(firstBike.getFuelType());
 
+
         System.out.println(firstCar.getNumberOfWheels());
         System.out.println(firstCar.getColor());
         System.out.println(firstCar.getEngineSize());
